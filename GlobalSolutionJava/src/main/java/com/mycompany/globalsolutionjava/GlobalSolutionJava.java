5d58495ee35ed8aa315f46c582a629427bff677a
guicvsub <rm552321@fiap.com.br>
2024-11-17 08:46:06 -0300

    esse e um teste

---
+++ GlobalSolutionJava/src/main/java/com/mycompany/globalsolutionjava/GlobalSolutionJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.globalsolutionjava;
+
+/**
+ *
+ * @author gui
+ */
+public class GlobalSolutionJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
