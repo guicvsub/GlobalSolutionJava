8d7cd0ff026cb7c0ec76278bd66fd8077070ed64
guicvsub <rm552321@fiap.com.br>
2024-11-17 09:24:57 -0300

    criando-clase-user-metodos

--- GlobalSolutionJava/src/main/java/models/Usuario.java
+++ GlobalSolutionJava/src/main/java/models/Usuario.java
@@ -6,5 +6,42 @@ package models;
  * @author gui
  */
 public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String Senha;
+
+    public Usuario(int id, String nome, String email, String Senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.Senha = Senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+    public void autenticar(String email, String senha )  {
     
+    }
+    public void registrarComodo(){
+        
+        
+    }
+    public void gerarRelatorio(){
+        
+        
+    }
 }
