2ebbe4761490dc5605de5e485f7413cfa0b64001
guicvsub <rm552321@fiap.com.br>
2024-11-17 09:11:08 -0300

    criando-classes

---
+++ GlobalSolutionJava/src/main/java/models/Entidade.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author gui
+ */
+public class Entidade {
+  
+    public void salvar() {
+        System.out.println("Entidade salva!");
+    }
+
+    public void atualizar() {
+        System.out.println("Entidade atualizada!");
+    }
+
+    public void deletar() {
+        System.out.println("Entidade deletada!");
+    }
+}
