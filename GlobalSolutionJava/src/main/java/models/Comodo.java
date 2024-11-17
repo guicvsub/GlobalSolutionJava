6abd21ac39c30256e3ccdf6898aac97c00230f2b
guicvsub <rm552321@fiap.com.br>
2024-11-17 10:32:06 -0300

    Criando-classe-comodo

--- GlobalSolutionJava/src/main/java/models/Comodo.java
+++ GlobalSolutionJava/src/main/java/models/Comodo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
 /**
@@ -9,5 +5,41 @@ package models;
  * @author gui
  */
 public class Comodo {
+    private int id; 
+    private String nomeComodo;
+    private Float area;
+
+    public Comodo(int id, String nomeComodo, Float area) {
+        this.id = id;
+        this.nomeComodo = nomeComodo;
+        this.area = area;
+    }
+
+    public Comodo(int id, String nomeComodo) {
+        this.id = id;
+        this.nomeComodo = nomeComodo;
+    }
+
+    public String getNomeComodo() {
+        return nomeComodo;
+    }
+
+    public Float getArea() {
+        return area;
+    }
+
+    public void setNomeComodo(String nomeComodo) {
+        this.nomeComodo = nomeComodo;
+    }
+
+    public void setArea(Float area) {
+        this.area = area;
+    }
+    public void adicionarDispositivo(){
+    
+    }
+    public void listarDispositivos(){
+    
+    }
     
 }
